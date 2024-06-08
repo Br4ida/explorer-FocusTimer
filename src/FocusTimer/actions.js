058fc96ff8908e77418266e2a487e73a4e4ce65a
Gabriel Braida <gabrielbnaza@gmail.com>
2024-06-07 23:56:39 -0300

    Adding setMinutes function

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -17,7 +17,7 @@ export function reset() {
 
 export function set() {
   el.minutes.setAttribute("contenteditable", true);
-  el.seconds.setAttribute("contenteditable", true);
+  el.minutes.focus();
 }
 
 export function toggleMusic() {
